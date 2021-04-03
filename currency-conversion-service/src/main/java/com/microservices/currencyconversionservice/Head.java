390c8940b4884016cb561b90b132e3087a05b050
Manjot <manjot.sokhi2107@gmail.com>
2021-04-03 16:30:20 -0400

    First Commit

---
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/Head.java
@@ -0,0 +1,12 @@
+package com.microservices.currencyconversionservice;
+
+public class Head {
+
+	public Head() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
